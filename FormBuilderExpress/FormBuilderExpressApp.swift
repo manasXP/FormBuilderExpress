cecd7c123ad6a8e619a7edb865d25c2201432716
Manas Pradhan <manaspr@gmail.com>
2025-07-07 06:40:29 +0530

    Initial Commit

---
+++ FormBuilderExpress/FormBuilderExpressApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormBuilderExpressApp.swift
+//  FormBuilderExpress
+//
+//  Created by Manas Pradhan on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FormBuilderExpressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
